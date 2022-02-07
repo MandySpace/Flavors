11240429fb972d917e4cd23ade4ea2f16d056306
MandySpace <amandeepsingh1994@gmail.com>
2022-02-07 12:18:07 +0530

    Responsive hero img

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,4 +1,7 @@
 import hero_5433 from "../../img/hero_img/original-5433.jpg";
+import hero_2400 from "../../img/hero_img/large-2400.jpg";
+import hero_1920 from "../../img/hero_img/medium-1920.jpg";
+import hero_640 from "../../img/hero_img/small-640.jpg";
 import { useNavigate } from "react-router-dom";
 
 function Hero() {
@@ -12,7 +15,8 @@ function Hero() {
     <div className="hero">
       <div className="hero-img">
         <img
-          src={hero_5433}
+          src={hero_640}
+          srcSet={`${hero_5433} 3500w, ${hero_2400} 2400w, ${hero_1920} 1920w, ${hero_640} 640w`}
           alt="Sweet delicacies on a serving board with black background"
           className="img-cover"
         />
